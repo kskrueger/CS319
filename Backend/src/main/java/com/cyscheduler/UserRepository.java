3040dfd950689358abc5682f0dbc59825407b81f
Karter Krueger <>
2019-10-31 16:19:59 -0500

    Backend initial setup!

--- Backend/src/main/java/com/cyscheduler/UserRepository.java
+++ Backend/src/main/java/com/cyscheduler/UserRepository.java
@@ -3,7 +3,6 @@ package com.cyscheduler;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.Collection;
 
 @Repository
