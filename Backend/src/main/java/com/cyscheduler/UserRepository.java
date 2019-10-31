fa250719046079d9eda9679792cc2f91112325fa
Karter Krueger <>
2019-10-31 16:18:32 -0500

    Backend initial setup!

---
+++ Backend/src/main/java/com/cyscheduler/UserRepository.java
@@ -0,0 +1,12 @@
+package com.cyscheduler;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Collection<User> findByUuid(@Param("uuid") Integer uuid);
+}
