4e88a2049933beedd11d2c3a2e75e1968acf2fd1
Daulton <djleach@iastate.edu>
2019-11-06 19:43:29 -0600

    Json not working

--- Frontend/CreateAccountJS.js
+++ Frontend/CreateAccountJS.js
@@ -3,23 +3,40 @@ function JSValidate(){
 	var password = document.getElementById("password");
 	var email = document.getElementById("email");
 	var major = document.getElementById("major");
+    var userList = [{}];
+    var xhr = new XMLHttpRequest();
+    var url = "http://coms-319-078.cs.iastate.edu:8080/user"; //HERE
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    //xhr.setRequestHeader("Content-Type", "application/json"); 
+    var pass = true;
 	if(!alphaNumCheck(username.value)){
         alert("Username Must contain only alphabetic or numeric characters.");
+        pass = false;//
 	}
     else if(username.value.length == 0 || username.value.length > 15){
         alert("Username must contain atleast 1 and at most 14 characters");
+        pass = false;
     }
     if(!checkEmail(email.value)){
         alert("Please Enter a valid email address");
+        pass = false;
 	}
 	if(!alphaNumCheck(password.value)){
         alert("Password must contain only alphabetic or numericcharacters.");
+        pass = false;
 	}
     else if(password.value.length < 8 || password.value.length > 15){
         alert("Password must be atleast 8 characters and at most 14 characters");
+        pass = false;
     }
     if(major.value == ""){
-        alert("Please Fill out the Major filed")
+        alert("Please Fill out the Major field")
+        pass = false;
+    }
+    if(pass == true){
+        var Json = JSON.stringify({Username: username.value, Password:password.value, Major:major.value, Email:email.value});
+        xhr.send(Json);
     }
     
 }
