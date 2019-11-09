8b96debe8903a50997436a3df0963668652966b0
Daulton <djleach@iastate.edu>
2019-11-09 11:08:01 -0600

    Login Verification works! Just need to test the GET method

--- Frontend/CySchedulerJs.js
+++ Frontend/CySchedulerJs.js
@@ -2,22 +2,25 @@ function login(){
     var username = document.getElementById("username");
 	var password = document.getElementById("password");
     var login = false;
-    var userList = [{}];
+    //TEST CODE
+//    var userList = [{"uuid":1,"username":"asdfasdf","password":"adsfasdf","major":"add","email":"adsf@asdf.com"},{"uuid":2,"username":"abcdef"},{"uuid":3,"username":"WORKING1DAB","password":"asdfadsf","major":"dabbb","email":"it@WORKS.dab"},{"uuid":4,"username":"Karterrrrr","password":"asdfghjkl","major":"As","email":"A@k.com"},{"uuid":5,"username":"djleach","password":"ddddddddddddd","major":"ddd","email":"Daultonleach@yahoo.com"},{"uuid":6,"username":"djleach","password":"password1","major":"SE","email":"Daultonleach@yahoo.com"}];
+     var userList = [{}];
     var url = "http://coms-319-078.cs.iastate:8080/users"; //HERE
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET", url, true);
     xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         userList = JSON.parse(this.responseText);
-        myFunction(myArr);
+        myFunction(userList);
         }
     };
+    myFunction(userList);
     xmlhttp.send();
 
 }
-function myFunction(arr){
+function myFunction(myArr){
     for(var i = 0; i < myArr.length; i++){
-        if(username.value == myArr[i].username && password.value == myArr[i].password.value){
+        if(username.value == myArr[i].username && password.value == myArr[i].password){
             login = true;
             break;
         }
