91f33007e89600d323ddb2a4eeacab1770be3b8a
Daulton <djleach@iastate.edu>
2019-11-09 10:34:34 -0600

    server login verification

--- Frontend/CySchedulerJs.js
+++ Frontend/CySchedulerJs.js
@@ -1,16 +1,34 @@
 function login(){
     var username = document.getElementById("username");
 	var password = document.getElementById("password");
-    var xhr = new XMLHttpRequest();
+    var login = false;
+    var userList = [{}];
     var url = "http://coms-319-078.cs.iastate:8080/users"; //HERE
-    xhr.onreadystatechange = function () { 
-        if (xhr.readyState === 4 && xhr.status === 200) { 
-  
-            var userList = JSON.parse(this.responseText);
-  
-        } 
-     };
-    xhr.open("GET", url, true);
-    xhr.send();
-    
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", url, true);
+    xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        userList = JSON.parse(this.responseText);
+        myFunction(myArr);
+        }
+    };
+    xmlhttp.send();
+
+}
+function myFunction(arr){
+    for(var i = 0; i < myArr.length; i++){
+        if(username.value == myArr[i].username && password.value == myArr[i].password.value){
+            login = true;
+            break;
+        }
+        else{
+            login = false;
+        }
+    }
+    if(login == true){
+        window.open("SchedulingPage.html");
+    }
+    else{
+        alert("Invalid Login Credentials");
+    }
 }
