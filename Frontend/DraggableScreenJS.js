3d67f62280f9359c9bac3d8a92539e150a2b3f00
Karter Krueger <>
2019-11-15 13:01:28 -0600

    Draggable

--- Frontend/DraggableScreenJS.js
+++ Frontend/DraggableScreenJS.js
@@ -1,3 +1,5 @@
+let gridX = 120;
+let gridY = 100;
 function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     if (document.getElementById(elmnt.id)) {
@@ -23,13 +25,18 @@ function dragElement(elmnt) {
         e = e || window.event;
         e.preventDefault();
         // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
+        pos1 = pos3 - e.clientX; // pos3 = initialTranslateX, pos1 = amount of change
         pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
+
         pos4 = e.clientY;
+        pos3 = e.clientX;
+
         // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        //dragContext.initialTranslateX + ((int) (mouseEvent.getX()) / gridX) * gridX - dragContext.mouseAnchorX
+        //elmnt.style.left = (elmnt.offsetLeft - Math.floor(pos3/gridX) * gridX) + "px";
+        //elmnt.style.top = (elmnt.offsetTop - Math.floor(pos4/gridY) * gridY) + "px"; // add current position + amount of change
         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px"; // add current position + amount of change
     }
 
     function closeDragElement() {
@@ -55,4 +62,12 @@ function addCourse() {
     for(let i = 0; i < childDivs.length; i++) {
         dragElement(childDivs[i]);
     }
+}
+
+function intDiv(a,b) {
+    let result = a/b;
+    if(result>=0)
+        return Math.floor(result);
+    else
+        return Math.ceil(result);
 }
